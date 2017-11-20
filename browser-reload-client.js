69f504eda9ff865d70f7d52ff081f6ae4b6d0648
LUXOFFICE\balazs.zubak <balazs.zubak@doclerholding.com>
2017-11-20 14:53:41 +0100

    fixed url port matching

--- browser-reload-client.js
+++ browser-reload-client.js
@@ -1,9 +1,33 @@
 console.log("browser-reload loaded");
 
+function getPort(url) {
+	url = url.match(/^(([a-z]+:)?(\/\/)?[^\/]+).*$/)[1] || url;
+	var parts = url.split(':'),
+		port = parseInt(parts[parts.length - 1], 10);
+	if(parts[0] === 'http' && (isNaN(port) || parts.length < 3)) {
+		return 80;
+	}
+	if(parts[0] === 'https' && (isNaN(port) || parts.length < 3)) {
+		return 443;
+	}
+	if(parts.length === 1 || isNaN(port)) return 80;
+	return port;
+}
+
+function getScriptPort()
+{
+	var scripts = document.getElementsByTagName('script');
+	for ( i in scripts )
+	{
+		if ( scripts[i].src && scripts[i].src.indexOf("/browser-reload-client.js") > -1 )
+			return getPort(scripts[i].src);
+	}
+}
+
 function listen()
 {
 	console.log("browser-reload listening...")
-	var socket = io.connect("http://localhost:3998");
+	var socket = io.connect("http://localhost:" + getScriptPort());
 	socket.on("hotswap", function()
 	{
 		location.reload();
@@ -13,14 +37,13 @@ function listen()
 if ( io === undefined ) {
 	var script = document.createElement('script');
 	script.onload = listen();
-	script.src = "/socket.io/socket.io.js";
+	script.src = "http://localhost:" + getScriptPort() + "/socket.io/socket.io.js";
 	document.body.appendChild(script);
 }
 else
 {
 	listen();
 }
-
 
 
 
